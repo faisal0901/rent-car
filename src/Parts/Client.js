127566b2dc15678f2c7b4377573de30ae0f7f92e
bob <muhfaisalll150402@gmail.com>
2021-05-11 07:33:02 +0700

    client

---
+++ src/Parts/Client.js
@@ -0,0 +1,39 @@
+import React from "react";
+import BwmIcon from "assets/images/bmw.png";
+import AudiIcon from "assets/images/audi.png";
+import HondaIcon from "assets/images/honda.png";
+import TeslaIcon from "assets/images/tesla.png";
+export default function Client() {
+  return (
+    <div className="clients flex flex-wrap justify-between items-center">
+      <div className="w-1/4">
+        <img
+          src={BwmIcon}
+          alt="rent a bwm"
+          className="w-full filter grayscale hover:filter-none image-client hover:grayscale-0 cursor-pointer"
+        />
+      </div>
+      <div className="w-1/4">
+        <img
+          src={AudiIcon}
+          alt="rent a bwm"
+          className="w-full filter grayscale hover:filter-none image-client hover:grayscale-0 cursor-pointer"
+        />
+      </div>
+      <div className="w-1/4">
+        <img
+          src={HondaIcon}
+          alt="rent a bwm"
+          className="w-full filter grayscale hover:filter-none image-client  hover:grayscale-0 cursor-pointer"
+        />
+      </div>
+      <div className="w-1/4">
+        <img
+          src={TeslaIcon}
+          alt="rent a bwm"
+          className="w-full filter grayscale hover:filter-none image-client hover:grayscale-0 cursor-pointer"
+        />
+      </div>
+    </div>
+  );
+}
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,6 +2,7 @@ import Header from "Parts/Header";
 import Hero from "Parts/Hero";
 import React from "react";
 import country from "json/Country.json";
+import Client from "Parts/Client";
 export default function LandingPage() {
   return (
     <>
@@ -10,6 +11,14 @@ export default function LandingPage() {
           <Header></Header>
           <Hero data={country}></Hero>
         </div>
+      </section>
+      <section className="mt-40">
+        <div className="container mx-auto">
+          <Client />
+        </div>
+      </section>
+      <section className="mt-40">
+        <div className="container mx-auto"></div>
       </section>
     </>
   );
